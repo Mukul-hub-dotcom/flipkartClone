e3b0e3e8598a1b00cba9c01cab5b9364987b5734
Mukul <65118538+Mukul-hub-dotcom@users.noreply.github.com>
2023-05-24 19:27:03 +0530

    added data for banner

---
+++ client/src/components/home/Banner.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import Carousel from "react-multi-carousel";
+import { styled } from "@mui/material";
+import { bannerData } from "../../constant/data";
+import "react-multi-carousel/lib/styles.css";
+
+const Image = styled("img")`
+  width: 100%;
+  height: 280px;
+`;
+
+const responsive = {
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 1,
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2,
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+  },
+};
+
+const Banner = () => {
+  return (
+    <Carousel
+      responsive={responsive}
+      containerClass="carousel-container"
+      autoPlay={true}
+      autoPlaySpeed={3000}
+      infinite={true}
+      swipeable={false}
+      draggable={false}
+      dotListClass="custom-dot-list-style"
+      itemClass="carousel-item-padding-40-px"
+    >
+      {bannerData.map((data) => (
+        <Image src={data.url} alt="banner" />
+      ))}
+    </Carousel>
+  );
+};
+
+export default Banner;
