74cd44aa43d94d9356110d9deb991d2b4fd76d45
Mukul <65118538+Mukul-hub-dotcom@users.noreply.github.com>
2023-05-24 19:20:58 +0530

    index file of react

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "./components/header/Header";
+import Home from "./components/home/Home";
+import { Box } from "@mui/material";
+import DataProvider from "./context/DataProvider";
+function App() {
+  return (
+    <DataProvider>
+      <Header />
+      <Box style={{ marginTop: '54px' }}>
+        <Home />
+      </Box>
+    </DataProvider>
+  );
+}
+
+export default App;
