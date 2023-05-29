d296c3dbe8779b239a12c45562e96a5e95a7d019
Mukul <ramdevmukul800@gmail.com>
2023-05-29 15:17:17 +0530

    getting products in redux store

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -1,12 +1,15 @@
 import axios from "axios";
 import * as actionTypes from "./constant";
+
 const URL = "http://localhost:8000";
-export const getProducts = async (dispatch) => {
-  try {
-    let { data } = await axios.get(`${URL}/products`);
-    console.log(data);
-    dispatch({ type: actionTypes.GetProductSuccess, payload: data });
-  } catch (error) {
-    dispatch({ type: actionTypes.GetProductFail, payload: error.message });
-  }
+
+export const getProducts = () => {
+  return async (dispatch) => {
+    try {
+      let { data } = await axios.get(`${URL}/products`);
+      dispatch({ type: actionTypes.GetProductSuccess, payload: data });
+    } catch (error) {
+      dispatch({ type: actionTypes.GetProductFail, payload: error.message });
+    }
+  };
 };
