66950c44ff2bc6d2a98ce0bfb1f756ce94daf824
Mukul <ramdevmukul800@gmail.com>
2023-05-30 15:40:05 +0530

    route for finding product from redux

--- server/routes/route.js
+++ server/routes/route.js
@@ -1,13 +1,16 @@
-import express from 'express'
-import { userSignup,userLogin } from '../controller/user-controller.js'
-import { getProducts } from '../controller/product-controller.js'
+import express from "express";
+import { userSignup, userLogin } from "../controller/user-controller.js";
+import {
+  getProducts,
+  getProductById,
+} from "../controller/product-controller.js";
 
-const router = express.Router()
+const router = express.Router();
 
-router.post('/signup',userSignup)
-router.post('/login',userLogin)
+router.post("/signup", userSignup);
+router.post("/login", userLogin);
 
-router.get('/products',getProducts)
+router.get("/products", getProducts);
+router.get("/product/:id", getProductById);
 
-
-export default router
+export default router;
