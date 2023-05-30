2b7ce8a5fd91f594a016f1f551db14cbf34879e6
Mukul <ramdevmukul800@gmail.com>
2023-05-30 10:56:57 +0530

    added ad banners in homepage

--- client/src/components/home/MidSlide.jsx
+++ client/src/components/home/MidSlide.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
-import { Slide } from './Slide'
-function MidSlide({products,title,timer}) {
+import React from "react";
+import { Slide } from "./Slide";
+import { Box, styled } from "@mui/material";
+function MidSlide({ products, title, timer }) {
+  const adURL =
+    "https://rukminim1.flixcart.com/flap/464/708/image/633789f7def60050.jpg?q=70";
+
+  const Component = styled(Box)`
+    display: flex;
+    alignitems: "center";
+  `;
+  const LeftComponent = styled(Box)(({ theme }) => ({
+    width: "83%",
+    [theme.breakpoints.down("md")]: {
+      width: "100%",
+    },
+  }));
+
+  const RightComponent = styled(Box)(({ theme }) => ({
+    background: "#ffffff",
+    padding: "5px",
+    marginTop: "10px",
+    marginLeft: "10px",
+    textAlign: "center",
+    [theme.breakpoints.down("md")]: { display: "none" },
+  }));
   return (
-    <div>
-      
-    </div>
-  )
+    <Component>
+      <LeftComponent>
+        <Slide products={products} title={title} timer={timer} />
+      </LeftComponent>
+      <RightComponent>
+        <img src={adURL} alt="" style={{ width: 217 }} />
+      </RightComponent>
+    </Component>
+  );
 }
 
-export default MidSlide
+export default MidSlide;
