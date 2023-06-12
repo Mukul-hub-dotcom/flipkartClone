f57060cf7e889358e09d6920047e89f9fb4cdfd2
Mukul <ramdevmukul800@gmail.com>
2023-06-12 14:03:26 +0530

    added search functionality

--- client/src/components/header/Search.jsx
+++ client/src/components/header/Search.jsx
@@ -1,7 +1,25 @@
-import React from "react";
-import { InputBase, Box } from "@mui/material";
+import React,{useState,useEffect} from "react";
+import { InputBase, Box, ListItem,List,styled } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
+import { useSelector,useDispatch } from "react-redux";
+import { getProducts } from "../../redux/actions";
+import { Link } from "react-router-dom";
+const ListWrapper=styled(List)`
+position:absolute;
+background:#ffffff;
+color:#000;
+margin-top:35px;
+`
 const Search = () => {
+  const [text,setText]=useState('')
+  const {products}=useSelector(state=>state.getProducts)
+  const dispatch=useDispatch()
+  const getText=(text)=>{
+setText(text)
+  }
+  useEffect(()=>{
+dispatch(getProducts())
+  },[dispatch])
   return (
     <div style={{ display: "flex" }}>
       <InputBase
@@ -11,7 +29,9 @@ const Search = () => {
           width: 300,
           height: 30,
         }}
+        
         placeholder="Search for brands and more...      "
+        onChange={(e)=>getText(e.target.value)}
       />
       <Box>
         <SearchIcon
@@ -22,7 +42,23 @@ const Search = () => {
             height: 30,
           }}
         />
+       
       </Box>
+      {
+        text &&
+        <ListWrapper>
+          {products.filter(product=>product.title.longTitle.toLowerCase().includes(text.toLowerCase())).map(product=>(
+            <ListItem>
+              <Link to={`/product/${product.id}`}
+                onClick={()=>setText('')}
+                style={{textDecoration:'none',color:'inherit'}}
+>
+              {product.title.longTitle}
+              </Link>
+            </ListItem>
+          ))}
+        </ListWrapper>
+      }
     </div>
   );
 };
